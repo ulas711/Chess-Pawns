05a0e4e2cd63dee7dfa6febe0b2830d1a2b13bd1
Ulaş Canımoğlu <73282600+ulas711@users.noreply.github.com>
2021-10-12 15:24:50 +0300

    Update deneme1.js

--- pawns/deneme1.js
+++ pawns/deneme1.js
@@ -1,16 +1,6 @@
 // İlk piyon beyaz olmak zorunda
 // 2 tane siyah piyon yan yana gelmemek zorunda
-// 8 tane piyonu satranç tahtasındaki satıra kaç farklı şekilde dizebiliriz.
-
-
-// 1 piyon = 1          8
-// 2 piyon = 2          16
-// 3 piyon = 3          24
-// 4 piyon = 5          40
-// 5 piyon = 8           
-// 6 piyon = 13           
-// 7 piyon = 21           
-// 8 piyon = 28           
+// 8 tane piyonu satranç tahtasındaki satıra kaç farklı şekilde dizebiliriz.          
 
 var all_satir = new Array(); 
 var satir = new Array();
@@ -64,6 +54,7 @@ function olustur (all_satir,satir,sayac)
 
 
 
+
 while (sayac!=34)
 {
     olustur(all_satir,satir, sayac);
@@ -72,34 +63,6 @@ while (sayac!=34)
 for (var i=0; i<34; i++)
 {
     console.log(all_satir[i]);
-    console.log("\n");
+    console.log(i+1);
 }
-
-
-// Eğer değer 1 ise rastgele ata eğer 0 ise 1 ata 
-
-
-
-// _ _ _ _ _ _ _ _ 
-// 2.2
-
-
-
-
-/*
-class Node
-{
-    constructor(item)
-    {
-        this.key = item;
-        this.left = this.right = null;
-    }
-}
-*/
-// başaramadık abi... 
-
-
-
-
-
 
